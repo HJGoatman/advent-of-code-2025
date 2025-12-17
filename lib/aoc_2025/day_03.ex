f3d3705da4a8d9a7983b41bf9c17f6d8ba3ece5f
Harry Goatman <hg@harrygoatman.com>
2025-12-17 16:05:04 +0000

    feat(3): part 2

--- lib/aoc_2025/day_03.ex
+++ lib/aoc_2025/day_03.ex
@@ -4,17 +4,26 @@ defmodule AOC2025.Day03 do
 
   @impl AOCDay
   def part_1(input) do
+    solve_part(input)
+  end
+
+  @part_2_num_batteries 12
+
+  @impl AOCDay
+  def part_2(input) do
+    solve_part(input, @part_2_num_batteries)
+  end
+
+  def solve_part(input, num_batteries \\ 2) do
     banks = parse_input(input)
     Logger.debug("banks: #{inspect(banks)}")
 
-    batteries_to_turn_on = banks |> Enum.map(&find_batteries_to_turn_on/1)
+    batteries_to_turn_on =
+      banks |> Enum.map(fn battery -> find_batteries_to_turn_on(battery, num_batteries) end)
+
     Logger.debug("batteries: #{inspect(batteries_to_turn_on, charlists: :as_lists)}")
 
     Enum.sum(batteries_to_turn_on) |> Integer.to_string()
-  end
-
-  @impl AOCDay
-  def part_2(_input) do
   end
 
   defp parse_input(input) do
@@ -27,36 +36,30 @@ defmodule AOC2025.Day03 do
   end
 
   @spec find_batteries_to_turn_on([integer()]) :: integer()
-  defp find_batteries_to_turn_on(bank) do
-    bank
+  defp find_batteries_to_turn_on(bank, num_batteries \\ 2) do
+    {remaining_bank, initial_state} = bank |> Enum.split(length(bank) - num_batteries)
+
+    remaining_bank
     |> Enum.reverse()
-    |> Enum.reduce({nil, nil}, fn battery, {left, right} ->
-      cond do
-        right == nil ->
-          {left, battery}
-
-        left == nil ->
-          {battery, right}
-
-        battery > left ->
-          if left > right do
-            {battery, left}
-          else
-            {battery, right}
-          end
-
-        battery == left ->
-          if battery > right do
-            {left, battery}
-          else
-            {left, right}
-          end
-
-        true ->
-          {left, right}
-      end
-    end)
-    |> Tuple.to_list()
+    |> Enum.reduce(initial_state, &handle_new_battery/2)
     |> Integer.undigits()
+  end
+
+  def handle_new_battery(_, state) when state == [], do: []
+
+  def handle_new_battery(battery, state) do
+    Logger.debug("battery, state: #{battery}, #{inspect(state)}")
+    [first | rest] = state
+
+    cond do
+      first == nil ->
+        [battery | rest]
+
+      battery >= first ->
+        [battery | handle_new_battery(first, rest)]
+
+      true ->
+        state
+    end
   end
 end
