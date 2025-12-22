1c4329c61b2dc9fc87d6f31a49a96e98bdc3e280
Harry Goatman <hg@harrygoatman.com>
2025-12-22 15:48:18 +0000

    feat(6): part 2

--- lib/aoc_2025/day_06.ex
+++ lib/aoc_2025/day_06.ex
@@ -27,15 +27,7 @@ defmodule AOC2025.Day06 do
 
     Logger.debug("transposed: #{inspect(transposed)}")
 
-    answers =
-      Enum.zip(transposed, parsed_operations)
-      |> Enum.map(fn {vals, op} ->
-        case op do
-          :add -> Enum.reduce(vals, 0, &Kernel.+/2)
-          :multiply -> Enum.reduce(vals, 1, &Kernel.*/2)
-        end
-      end)
-
+    answers = apply_operations(transposed, parsed_operations)
     Logger.debug("answers: #{inspect(answers)}")
 
     Enum.sum(answers) |> Integer.to_string()
@@ -43,5 +35,56 @@ defmodule AOC2025.Day06 do
 
   @impl AOCDay
   def part_2(input) do
+    {values, [operations | _]} =
+      input
+      |> String.split("\n")
+      |> Enum.split(-1)
+
+    transposed =
+      values
+      |> Enum.map(&String.graphemes/1)
+      |> Enum.zip_reduce([], fn elements, acc -> [elements | acc] end)
+      |> Enum.reverse()
+
+    Logger.debug("transposed: #{inspect(transposed)}")
+
+    parsed_operations =
+      operations
+      |> String.split()
+      |> Enum.map(fn op ->
+        case op do
+          "+" -> :add
+          "*" -> :multiply
+        end
+      end)
+
+    colwise =
+      transposed
+      |> Stream.chunk_by(fn c -> Enum.all?(c, fn v -> v == " " end) end)
+      |> Stream.filter(fn cs ->
+        not Enum.all?(cs, fn c -> Enum.all?(c, fn v -> v == " " end) end)
+      end)
+      |> Enum.map(fn col ->
+        Enum.map(col, fn val ->
+          Enum.filter(val, fn digit -> digit != " " end)
+          |> Enum.join("")
+        end)
+        |> Enum.map(&String.to_integer/1)
+      end)
+
+    answers = apply_operations(colwise, parsed_operations)
+    Logger.debug("answers: #{inspect(answers)}")
+
+    Enum.sum(answers) |> Integer.to_string()
+  end
+
+  defp apply_operations(values, operations) do
+    Enum.zip(values, operations)
+    |> Enum.map(fn {vals, op} ->
+      case op do
+        :add -> Enum.reduce(vals, 0, &Kernel.+/2)
+        :multiply -> Enum.reduce(vals, 1, &Kernel.*/2)
+      end
+    end)
   end
 end
